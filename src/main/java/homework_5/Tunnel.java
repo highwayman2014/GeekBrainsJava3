d92e65246b0b1b64fd4b705317996cdd9be8706f
highwayman2014 <shepardalex2012@gmail.com>
2021-05-02 12:05:32 +0300

    finish hw5

--- src/main/java/homework_5/Tunnel.java
+++ src/main/java/homework_5/Tunnel.java
@@ -1,21 +1,27 @@
 package homework_5;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage{
-    public Tunnel() {
+    private Semaphore goToTunnelSemaphore;
+    public Tunnel(Semaphore goToTunnelSemaphore) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+        this.goToTunnelSemaphore = goToTunnelSemaphore;
     }
     @Override
     public void go(Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                goToTunnelSemaphore.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                goToTunnelSemaphore.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
